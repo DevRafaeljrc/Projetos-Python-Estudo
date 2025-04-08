ab35398cd8cb0a862fba552e4c8ed08babe0b7db
Devrafaeljrc <Devrafaeljrc@outlook.com>
2025-04-07 22:32:35 -0300

    primeiro commit

---
+++ Aulas/Aula2.py
@@ -0,0 +1,34 @@
+#Tipos de dados
+
+#Inteiros
+idade = 20
+print(idade)
+
+#Float
+altura = 1.75
+print(altura)
+
+#String
+nome = "Lucas"
+print(nome)
+
+#Booleano
+maior_de_idade = True
+print(maior_de_idade)
+
+#Listas
+lista = [1, 2, 3, 4, 5]
+print(lista)
+
+#Tuplas
+tupla = (1, 2, 3, 4, 5)
+print(tupla)
+
+#Dicionários
+dicionario = {"nome": "Lucas", "idade": 20, "altura": 1.75}
+print(dicionario)
+
+#Conjuntos
+conjunto = {1, 2, 3, 4, 5}
+print(conjunto)
+
